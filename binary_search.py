e3bfaa7367e23ffeb268ca57a2b46e286aff26de
xwsa568 <xwsa568@gmail.com>
2025-06-15 21:23:34 +0900

    feat: implement binary search algorithm

---
+++ binary_search.py
@@ -0,0 +1,16 @@
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        if arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+if __name__ == "__main__":
+    data = [1, 3, 5, 7, 9, 11, 13]
+    print(binary_search(data, 7))
+    print(binary_search(data, 4))
